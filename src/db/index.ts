695381682fafc249784b6f966b25649aad33585b
Duncan Leo <hello@duncanleo.me>
2022-11-26 20:20:36 +0800

    chore(db): add `prisma` export for Next.js dev mode

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+/**
+ * See https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+ */
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
