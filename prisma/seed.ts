c8691483423e83b1aca75b830323a84966e864ec
Duncan Leo <hello@duncanleo.me>
2022-12-08 22:58:51 +0800

    fix: add placeholder seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+async function run() {
+  console.log('seed placeholder');
+}
+
+run()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
