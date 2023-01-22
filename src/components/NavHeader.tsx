8e5eff14062107bd9960a19b6d80dd79914585bc
kryeoh <89062427+kryeoh@users.noreply.github.com>
2023-01-22 23:25:15 +0800

    fix(navHeader): Update navheader for CM [BIMST08-101] (#20)
    * fix: added static navheader for cm
    
    * fix: updated svg with heroicons

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,69 +1,16 @@
+import { BellIcon, CogIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import React from 'react';
 
 const NavHeader: React.FC = function () {
   return (
-    <header className="flex justify-between items-center px-6 py-2 border-solid border border-gray-300">
+    <header className="flex justify-end items-center py-2 pr-6 border-solid border border-gray-300">
       <div className="flex">
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
+        <div className="py-3 pr-3">
+          <CogIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
         </div>
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
-        </div>
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
-        </div>
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
-        </div>
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
-        </div>
-        <div className="py-2 px-4">
-          <h1>TAB</h1>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="p-3">
-          <Image
-            src="/star.png"
-            alt="icon placeholder"
-            width={20}
-            height={19}
-          />
-        </div>
-        <div className="p-3">
-          <Image
-            src="/star.png"
-            alt="icon placeholder"
-            width={20}
-            height={19}
-          />
-        </div>
-        <div className="p-3">
-          <Image
-            src="/star.png"
-            alt="icon placeholder"
-            width={20}
-            height={19}
-          />
-        </div>
-        <div className="p-3">
-          <Image
-            src="/star.png"
-            alt="icon placeholder"
-            width={20}
-            height={19}
-          />
-        </div>
-        <div className="p-3">
-          <Image
-            src="/star.png"
-            alt="icon placeholder"
-            width={20}
-            height={19}
-          />
+        <div className="py-3 pr-3">
+          <BellIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
         </div>
         <span className="rounded-full w-10 h-10 bg-neutral-300 flex justify-center items-center">
           <Image
