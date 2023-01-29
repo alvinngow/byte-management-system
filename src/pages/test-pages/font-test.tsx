fa90dbaf530ffb356c79d5c6a8ca4ac85c30352b
icedbluw <alvinngow21@gmail.com>
2023-01-29 23:11:49 +0800

    style: setup Colors and typography [BIMST08-98] [BIMST08-99] (#33)
    * Typography and Colour config
    
    * Added font visby
    Added test pages folder
    Added font test page
    
    * Added visby to global css
    
    * updated header elements to use tailwind font size

---
+++ src/pages/test-pages/font-test.tsx
@@ -0,0 +1,156 @@
+import React from 'react';
+
+function FontTest() {
+  return (
+    <div>
+      <h1>Generated from: http://www.cufonfonts.com</h1>
+      <br />
+      <h1
+        style={{
+          fontFamily: 'Visby Regular',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Bold Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Bold',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Extrabold Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Extrabold',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Heavy Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Heavy',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Light Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Light',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Medium Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Medium',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Semibold Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Semibold',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Thin Italic',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+      <h1
+        style={{
+          fontFamily: 'Visby Thin',
+          fontWeight: 'normal',
+          fontSize: '42px',
+        }}
+      >
+        AaBbCcDdEeFfGgHhŞşIıİi Example
+      </h1>
+    </div>
+  );
+}
+
+export default FontTest;
