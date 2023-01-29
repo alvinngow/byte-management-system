0b3d1606a685d5d9aa8a1d0fa7350b82fae25d44
kryeoh <89062427+kryeoh@users.noreply.github.com>
2023-01-29 12:32:01 +0800

    fix(/manage/class): Made UI mobile responsive [BIMST08-81] (#45)
    * fix: redesigned according to figma
    
    * fix: made table mobile responsive

--- src/pages/manage/class/index.tsx
+++ src/pages/manage/class/index.tsx
@@ -1,3 +1,4 @@
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { NextPage } from 'next';
 import Image from 'next/image';
 
@@ -9,130 +10,253 @@ const ClassPage: React.FC = function () {
   return (
     <AppLayout>
       <NavHeader />
-      <div className="my-6 mx-auto flex w-4/5">
-        <div className="flex basis-4/5 flex-col">
-          <h6 className="mb-2 font-bold">Classes</h6>
-          <div className="flex">
-            <Image
-              className="h-4"
-              src="/star.png"
-              alt="icon placeholder"
-              width={16}
-              height={16}
-            />
-            <p className="mx-2">Home</p>
-            <span className="mx-2">/</span>
-            <span className="mx-2">...</span>
-            <span className="mx-2">/</span>
-            <Image
-              className="h-4"
-              src="/star.png"
-              alt="icon placeholder"
-              width={16}
-              height={16}
-            />
-            <p className="mx-2">Classes</p>
-          </div>
-        </div>
-        <button className="basis-1/5 rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
+      <div className="my-6 mx-auto flex w-4/5 items-center justify-between">
+        <h6 className="font-bold">Classes</h6>
+        <button className="rounded-full bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700">
           + ADD CLASS
         </button>
       </div>
-      <div className="mx-auto flex w-4/5">
-        <table className="table-auto border border-slate-300">
+      <div className="mx-auto flex w-80 overflow-x-auto sm:w-[30rem] md:w-min lg:w-4/5">
+        <table className="border border-slate-300 md:w-full">
           <thead>
             <tr>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 ID
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Title
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Location
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Trainer
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Date
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Student Attendees
               </th>
-              <th className="border border-slate-300 py-4 pl-4 text-left">
+              <th className="border-b border-slate-300 py-4 pl-4 text-left">
                 Volunteer Attendees
               </th>
+              <th className="border-b border-slate-300 py-4 pl-4 text-left" />
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 ID placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                1
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Title placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Learn Music Production From Scratch
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Location placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Sembawang CC
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Trainer placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Date placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Student Attendees placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #1 Volunteer Attendees placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
               </td>
-            </tr>
-            <tr>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 ID placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Title placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Location placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Trainer placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Date placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Student Attendees placeholder
-              </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #2 Volunteer Attendees placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
               </td>
             </tr>
             <tr>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 ID placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                2
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Title placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                The Complete Stock Trading Course 2023
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Location placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Ngee Ann Polytechnic
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Trainer placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Date placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Student Attendees placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
               </td>
-              <td className="border border-slate-300 py-4 pl-4 text-left">
-                #3 Volunteer Attendees placeholder
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                3
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Live Coding Masterclass: Beginners To Advanced
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Singapore Management University
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                How To Become A Gardening Pro In 30 Days
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Singapore Management University
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                5
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Microsoft Excel Ninja Course
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Singapore Management University
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                6
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Learn Music Production From Scratch
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                Singapore Management University
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                John Doe
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                26 Nov 2023
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                4
+              </td>
+              <td className="border-b border-slate-300 py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="py-4 pl-4 text-left">7</td>
+              <td className="py-4 pl-4 text-left">
+                The Complete Stock Trading Course 2023
+              </td>
+              <td className="py-4 pl-4 text-left">
+                Singapore Management University
+              </td>
+              <td className="py-4 pl-4 text-left">John Doe</td>
+              <td className="py-4 pl-4 text-left">26 Nov 2023</td>
+              <td className="py-4 pl-4 text-left">4</td>
+              <td className="py-4 pl-4 text-left">4</td>
+              <td className="py-4 pl-4 text-left">
+                <div className="flex gap-2.5">
+                  <PencilIcon
+                    style={{ color: '#6B7280' }}
+                    className="h-6 w-6"
+                  />
+                  <TrashIcon style={{ color: '#6B7280' }} className="h-6 w-6" />
+                </div>
               </td>
             </tr>
           </tbody>
