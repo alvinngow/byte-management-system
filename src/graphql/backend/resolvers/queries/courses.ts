09d876879130c2ebd6668902b7fbd3755ce32777
Duncan Leo <hello@duncanleo.me>
2023-02-10 21:37:24 +0800

    feat(graphql): filtering and sorting for `courses` query [BIMST08-32] (#67)

--- src/graphql/backend/resolvers/queries/courses.ts
+++ src/graphql/backend/resolvers/queries/courses.ts
@@ -1,8 +1,17 @@
 import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
 
-import { QueryResolvers } from '../../../../../gen/graphql/resolvers';
+import {
+  CourseDateFiltering,
+  CourseSortKey,
+  QueryResolvers,
+} from '../../../../../gen/graphql/resolvers';
 import { prisma } from '../../../../db';
 import requireAuthenticated from '../util/requireAuthenticated';
+import { Prisma } from '.prisma/client';
+import CourseWhereInput = Prisma.CourseWhereInput;
+import { DateTime } from 'luxon';
+import CourseWithSessionInfoOrderByWithRelationInput = Prisma.CourseWithSessionInfoOrderByWithRelationInput;
+import CourseWithSessionInfoWhereInput = Prisma.CourseWithSessionInfoWhereInput;
 
 export const coursesResolver: QueryResolvers['courses'] = async (
   root,
@@ -12,11 +21,75 @@ export const coursesResolver: QueryResolvers['courses'] = async (
 ) => {
   await requireAuthenticated(context);
 
-  const { first, after } = args;
+  const { first, after, filter, sortKey, reverse } = args;
+
+  let orderBy: CourseWithSessionInfoOrderByWithRelationInput | undefined =
+    undefined;
+
+  switch (sortKey) {
+    case CourseSortKey.StartDate: {
+      orderBy = {
+        firstSessionStartDate: reverse ? 'desc' : 'asc',
+      };
+      break;
+    }
+    case CourseSortKey.EndDate: {
+      orderBy = {
+        lastSessionEndDate: reverse ? 'desc' : 'asc',
+      };
+      break;
+    }
+    case CourseSortKey.Name: {
+      orderBy = {
+        name: reverse ? 'desc' : 'asc',
+      };
+      break;
+    }
+    case CourseSortKey.LocationName: {
+      orderBy = {
+        //@ts-ignore
+        defaultLocation: {
+          name: reverse ? 'desc' : 'asc',
+        },
+      };
+      break;
+    }
+  }
+
+  let where: CourseWithSessionInfoWhereInput | undefined = undefined;
+
+  if (filter != null) {
+    switch (filter.date) {
+      case CourseDateFiltering.Upcoming: {
+        where = {
+          firstSessionStartDate: {
+            gte: DateTime.now().toJSDate(),
+          },
+        };
+        break;
+      }
+      case CourseDateFiltering.Past: {
+        where = {
+          lastSessionEndDate: {
+            lt: DateTime.now().toJSDate(),
+          },
+        };
+        break;
+      }
+    }
+  }
 
   const result = await findManyCursorConnection(
-    (args) => prisma.course.findMany(args),
-    () => prisma.course.count(),
+    (args) =>
+      prisma.courseWithSessionInfo.findMany({
+        ...args,
+        orderBy,
+        where,
+      }),
+    () =>
+      prisma.courseWithSessionInfo.count({
+        where,
+      }),
     { first, after },
     { resolveInfo: info }
   );
--- tests/unit/courses.spec.ts
+++ tests/unit/courses.spec.ts
@@ -1,6 +1,11 @@
 import { gql } from '@apollo/client';
 
-import { CourseConnection } from '../../gen/graphql/resolvers';
+import {
+  CourseConnection,
+  CourseDateFiltering,
+  CourseFiltering,
+  CourseSortKey,
+} from '../../gen/graphql/resolvers';
 import { buildMockContext } from '../util/buildMockContext';
 import setupApolloServer from '../util/setupApolloServer';
 
@@ -13,6 +18,8 @@ interface Data {
 interface Variables {
   first?: number;
   after?: string;
+  sortKey?: CourseSortKey;
+  filter?: CourseFiltering;
 }
 
 const testServer = setupApolloServer();
@@ -91,5 +98,109 @@ describe('courses', () => {
     }
 
     expect(response.body.singleResult.data.courses?.edges.length).toBe(2);
+  });
+
+  test('sorting', async () => {
+    const response = await testServer.executeOperation<Data, Variables>(
+      {
+        query: gql`
+          query CoursesQuery(
+            $first: Int
+            $after: String
+            $sortKey: CourseSortKey
+          ) {
+            courses(first: $first, after: $after, sortKey: $sortKey) {
+              edges {
+                node {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          first: 2,
+          sortKey: CourseSortKey.Name,
+        },
+      },
+      {
+        contextValue: buildMockContext(USER_ID_USER),
+      }
+    );
+
+    expect(response.body.kind).toBe('single');
+    if (response.body.kind !== 'single') {
+      throw new Error();
+    }
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data).not.toBeNull();
+
+    if (response.body.singleResult.data == null) {
+      throw new Error();
+    }
+
+    const edges = response.body.singleResult.data.courses?.edges ?? [];
+
+    expect(edges).toStrictEqual(
+      [...edges].sort((prevEdge, nextEdge) => {
+        return prevEdge.node.name.localeCompare(nextEdge.node.name);
+      })
+    );
+  });
+
+  test('filtering', async () => {
+    const response = await testServer.executeOperation<Data, Variables>(
+      {
+        query: gql`
+          query CoursesQuery(
+            $first: Int
+            $after: String
+            $filter: CourseFiltering
+          ) {
+            courses(first: $first, after: $after, filter: $filter) {
+              edges {
+                node {
+                  id
+                  name
+                  defaultStartTime
+                  defaultEndTime
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          first: 2,
+          filter: {
+            date: CourseDateFiltering.Past,
+          },
+        },
+      },
+      {
+        contextValue: buildMockContext(USER_ID_USER),
+      }
+    );
+
+    expect(response.body.kind).toBe('single');
+    if (response.body.kind !== 'single') {
+      throw new Error();
+    }
+
+    expect(response.body.singleResult.errors).toBeUndefined();
+    expect(response.body.singleResult.data).not.toBeNull();
+
+    if (response.body.singleResult.data == null) {
+      throw new Error();
+    }
+
+    const edges = response.body.singleResult.data.courses?.edges ?? [];
+
+    expect(edges).toStrictEqual(
+      [...edges].sort((prevEdge, nextEdge) => {
+        return prevEdge.node.name.localeCompare(nextEdge.node.name);
+      })
+    );
   });
 });
