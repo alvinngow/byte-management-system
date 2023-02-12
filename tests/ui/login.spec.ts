25950124b88240b5efd052b3128f9df27178ac89
Duncan Leo <hello@duncanleo.me>
2023-02-12 23:15:49 +0800

    feat(tests): Login UI test for volunteer [BIMST08-29] (#87)

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,13 +1,27 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 
-test('login', async ({ page }) => {
+async function login(page: Page, username: string, password: string) {
   await page.goto('http://localhost:3000/login');
-  await page.locator('input[name="email"]').fill('byte@bims.com');
-  await page.locator('input[name="password"]').fill('bytelovesbims');
+  await page.locator('input[name="email"]').fill(username);
+  await page.locator('input[name="password"]').fill(password);
   await page.getByRole('button', { name: 'Log in' }).click();
 
   await expect(page).toHaveURL('http://localhost:3000/home');
   await expect(page.locator('h1')).toHaveText(
     'Discover Causes (Courses) That Matter To You'
   );
+}
+
+test.describe('login', () => {
+  test('system admin', async ({ page }) => {
+    await login(page, 'byte@bims.com', 'bytelovesbims');
+  });
+
+  test('committee member', async ({ page }) => {
+    await login(page, 'jessica@bims.com', 'jessicaloveshamsters');
+  });
+
+  test('volunteer', async ({ page }) => {
+    await login(page, 'emily@bims.com', 'emilyinbims');
+  });
 });
