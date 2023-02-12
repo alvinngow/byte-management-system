c1ec29ef0d8cc0283e0794bfad1a212d67331deb
kryeoh <89062427+kryeoh@users.noreply.github.com>
2023-02-12 19:46:46 +0800

    feat(/discover-courses/detail/[id]): Create static UI [BIMST08-121] [BIMST08-150] (#79)

---
+++ src/pages/discover-courses/index.tsx
@@ -0,0 +1,10 @@
+const DiscoverCoursesPage: React.FC = function () {
+  return (
+    <>
+      <h1>TEMP PLACEHOLDER</h1>
+      <h2>for Discover Courses</h2>
+    </>
+  );
+};
+
+export default DiscoverCoursesPage;
