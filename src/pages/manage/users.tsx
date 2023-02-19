ab66a74ad3c29c2deb9af049179a5317295a24dd
jessicanovi <76735135+jessicanovi@users.noreply.github.com>
2023-02-19 23:06:30 +0800

    UI: Static Manage User Page [BIMST08-40][BIMST08-147][BIMST08-163][BIMST08-85][BIMST08-132] (#102)

--- src/pages/manage/users.tsx
+++ src/pages/manage/users.tsx
@@ -1,7 +1,6 @@
 import { useMutation, useQuery } from '@apollo/client';
 import {
   ArrowsUpDownIcon,
-  ChevronDownIcon,
   EllipsisVerticalIcon,
 } from '@heroicons/react/24/outline';
 import { NextPage } from 'next';
@@ -11,8 +10,11 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { UserRole } from '../../../gen/graphql/resolvers';
+import IconButton from '../../components/IconButton';
+import Input from '../../components/Input';
 import NavBar from '../../components/NavBar';
 import NavHeader from '../../components/NavHeader';
+import Select from '../../components/Select';
 import * as AccountRoleUpdate from '../../graphql/frontend/mutations/AccountRoleUpdateMutation';
 import * as UsersQuery from '../../graphql/frontend/queries/UsersQuery';
 import useCurrentUser from '../../hooks/useCurrentUser';
@@ -37,6 +39,7 @@ const UsersPage: NextPage = function (props) {
   >(AccountRoleUpdate.Mutation);
 
   const router = useRouter();
+  const [modal, setModal] = React.useState(false);
 
   React.useEffect(() => {
     if (me == null) {
@@ -70,6 +73,17 @@ const UsersPage: NextPage = function (props) {
     },
     [accountRoleUpdate]
   );
+  const handleClick = () => {
+    if (modal) {
+      setModal(false);
+    } else {
+      setModal(true);
+    }
+    console.log(modal);
+  };
+  const handleBackdropClick: React.MouseEventHandler<HTMLDivElement> = () => {
+    setModal(false);
+  };
 
   if (meLoading) {
     return null;
@@ -77,87 +91,92 @@ const UsersPage: NextPage = function (props) {
 
   return (
     <AppLayout>
-      <NavHeader></NavHeader>
       {/* start of body DIV*/}
       <NavBar>
+        <NavHeader />
         <div>
           <div>
-            <h1 className="mb-6 mt-4 flex p-4 px-12 text-3xl">Users</h1>
+            <h4 className="mb-6 mt-4 flex p-4 px-12">Users</h4>
           </div>
           {/* start of table */}
           <div className="border-grey-400 ml-12 mr-12 mb-12 border-2 pl-5 pr-5">
             {/* search bar */}
             <div className="mb-2 grid grid-cols-2 gap-4 pl-5 pr-5 pt-6 pb-6">
               <div>
-                <p className="mb-1 text-xs text-gray-500">Search</p>
-                <button
-                  id="dropdownDefaultButton"
-                  data-dropdown-toggle="dropdown"
-                  className="inline-flex w-full items-center whitespace-pre border-b-2 border-solid border-b-neutral-400 py-2.5 text-center text-sm text-gray-500"
-                  type="button"
-                >
-                  Name, Email, School...{' '}
-                </button>
+                <Input label="Search" placeholder={'Name, Email, School... '} />
               </div>
-              <div>
-                <p className="text-xs text-gray-500">User Type</p>
-                <button
-                  id="dropdownDefaultButton"
-                  data-dropdown-toggle="dropdown"
-                  className="inline-flex w-full items-center border-b-2 border-solid border-b-neutral-400 py-2.5 text-center text-sm text-gray-500"
-                  type="button"
-                >
-                  All
-                  <ChevronDownIcon className="ml-auto h-6 w-6"></ChevronDownIcon>
-                </button>
+              <div className="relative">
+                <Select
+                  items={[
+                    { label: 'All', value: 'All' },
+                    { label: 'Volunteer', value: 'Volunteer' },
+                    { label: 'Committee Member', value: 'Committee Member' },
+                    { label: 'Admin', value: 'Admin' },
+                  ]}
+                  label={'User Type'}
+                  value={'All'}
+                  className="relative mb-3 w-full"
+                  onChange={function (value: string): void {
+                    throw new Error('Function not implemented.');
+                  }}
+                />
               </div>
             </div>
             {/* start of the user's details table */}
             <div className="snap-x overflow-x-auto scroll-smooth">
               {loading && <span>Loading</span>}
 
-              <table className="w-full text-left text-gray-500">
-                <thead className="text-sm text-gray-700">
-                  {/* need to add the up-down arrow icon */}
-                  <th className="columns-1 px-6 py-3">
-                    User Name{' '}
-                    <button className="">
-                      <span className="inline-flex content-center text-center">
-                        <ArrowsUpDownIcon className="h-5 w-5"></ArrowsUpDownIcon>
-                      </span>
-                    </button>
+              <table className="w-full table-auto text-left">
+                <thead className="subtitle2 py-auto">
+                  <th className="px-6 py-3">
+                    <div className="flex gap-1">
+                      <p className="whitespace-nowrap">User Name</p>
+                      <IconButton
+                        HeroIcon={(props) => (
+                          <ArrowsUpDownIcon className="h-5 w-5" />
+                        )}
+                      />
+                    </div>
                   </th>
-                  <th className="columns-1 px-6 py-3">
-                    Contact Number{' '}
-                    <button>
-                      <span className="inline-flex">
-                        <ArrowsUpDownIcon className="h-5 w-5"></ArrowsUpDownIcon>
-                      </span>
-                    </button>
+                  <th className="px-6 py-3">
+                    <div className="flex gap-1">
+                      <p className="whitespace-nowrap">Contact Number</p>
+                      <IconButton
+                        HeroIcon={(props) => (
+                          <ArrowsUpDownIcon className="h-5 w-5" />
+                        )}
+                      />
+                    </div>
                   </th>
-                  <th className="columns-1 px-6 py-3">
-                    School/ Work{' '}
-                    <button>
-                      <span className="inline-flex">
-                        <ArrowsUpDownIcon className="h-5 w-5"></ArrowsUpDownIcon>
-                      </span>
-                    </button>
+                  <th className="px-6 py-3">
+                    <div className="flex gap-1">
+                      <p className="whitespace-nowrap">School/ Work</p>
+                      <IconButton
+                        HeroIcon={(props) => (
+                          <ArrowsUpDownIcon className="h-5 w-5" />
+                        )}
+                      />
+                    </div>
                   </th>
-                  <th className="columns-1 px-6 py-3">
-                    User Type{' '}
-                    <button>
-                      <span className="inline-flex">
-                        <ArrowsUpDownIcon className="h-5 w-5"></ArrowsUpDownIcon>
-                      </span>
-                    </button>
+                  <th className="px-6 py-3">
+                    <div className="flex gap-1">
+                      <p className="whitespace-nowrap">User Type</p>
+                      <IconButton
+                        HeroIcon={(props) => (
+                          <ArrowsUpDownIcon className="h-5 w-5" />
+                        )}
+                      />
+                    </div>
                   </th>
-                  <th className="columns-1 px-6 py-3">
-                    Account Status{' '}
-                    <button>
-                      <span className="inline-flex">
-                        <ArrowsUpDownIcon className="h-5 w-5"></ArrowsUpDownIcon>
-                      </span>
-                    </button>
+                  <th className="px-6 py-3">
+                    <div className="flex gap-1">
+                      <p className="whitespace-nowrap">Account Status</p>
+                      <IconButton
+                        HeroIcon={(props) => (
+                          <ArrowsUpDownIcon className="h-5 w-5" />
+                        )}
+                      />
+                    </div>
                   </th>
                   {me?.role === UserRole.SystemAdministrator && (
                     <th className="px-6 py-3">Management</th>
@@ -166,114 +185,94 @@ const UsersPage: NextPage = function (props) {
                 <tbody>
                   {data?.users?.edges?.map((edge) => (
                     <tr key={edge.node.id} className="border-b bg-white">
-                      <td className="grid grid-cols-2 gap-2 px-6 py-4 text-black">
-                        {/* avatar is not appearing */}
-                        <div className="col-span-1 ml-0">
+                      <td className="body2 flex items-center gap-2 px-6 py-4 text-black">
+                        <div className="w-[40px]">
                           <Image
-                            className="h-10 w-10 rounded-full"
+                            className="h-[40px] w-[40px] rounded-full"
                             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU1wZeXDnAu6XK5bB6UsG69B-b2P_gnQfsfw&usqp=CAU"
-                            alt="Rounded avatar"
+                            alt="avatar-image"
                           />
+                          {/* avatar is not appearing */}
                           {edge.node.avatar}
                         </div>
-                        <div className="col-span-1 mr-auto">
-                          {edge.node.firstName} {edge.node.lastName}
-                          <br />
-                          <span className="text-gray-500">
+                        <div className="flex flex-col">
+                          <span>
+                            {edge.node.firstName} {edge.node.lastName}
+                          </span>
+                          <span className="text-secondary">
                             {edge.node.email}
                           </span>
                         </div>
                       </td>
-                      <td className="px-6 py-4 text-black">
-                        {edge.node.mobileNo}
-                      </td>
-                      <td className="px-6 py-4 text-black">
+                      <td className="px-6">{edge.node.mobileNo}</td>
+                      <td className="px-6">
                         {edge.node.school?.name ?? 'N/A'}
                       </td>
-                      <td className="px-6 py-4 text-black">{edge.node.role}</td>
-                      <td className="px-6 py-4 text-red-500">
-                        NOT IMPLEMENTED IN GRAPHQL
-                      </td>
-                      <td className="px-6 py-4 text-black">
-                        <button
-                          type="button"
-                          // id="ellipsisDropdown"
-                          id="dropdownDefaultButton"
-                          data-dropdown-toggle="dropdown"
-                        >
-                          <EllipsisVerticalIcon
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                          ></EllipsisVerticalIcon>
-                        </button>
-                        {/* dropdown code starts here */}
-                        <div
-                          id="dropdown"
-                          className=" w-44 divide-y divide-gray-100 rounded-lg text-gray-700 shadow "
-                        >
-                          <ul
-                            className="py-2 text-sm text-gray-700"
-                            aria-labelledby="dropdownDefaultButton"
-                          >
-                            <li>
-                              <button className=" px-4 py-2 text-left text-gray-700 hover:bg-gray-100">
-                                Make as Comm Member
-                              </button>
-                            </li>
-                            <li>
-                              <button className=" px-4 py-2 text-left text-gray-700 hover:bg-gray-100">
-                                Make as System Admin
-                              </button>
-                            </li>
-                            <li>
-                              <button className=" px-4 py-2 text-left text-gray-700 hover:bg-gray-100">
-                                Make as Volunteer
-                              </button>
-                            </li>
-                            <li>
-                              <button className=" px-4 py-2 text-left text-red-500 hover:bg-gray-100">
-                                Delete User
-                              </button>
-                            </li>
-                          </ul>
+                      <td className="px-6">{edge.node.role}</td>
+                      <td className="px-6 text-red-500">NOT IN GRAPHQL</td>
+                      <td className="relative">
+                        <div onClick={handleClick}>
+                          <IconButton
+                            HeroIcon={() => (
+                              <EllipsisVerticalIcon className="h-5 w-5" />
+                            )}
+                          />
                         </div>
+
+                        {/* dropdown is hidden for now */}
+                        {modal && (
+                          <div
+                            key={edge.node.id}
+                            className="absolute top-0 left-10 z-50 flex flex-col items-start gap-y-px overflow-hidden rounded-b-lg bg-white p-2.5 shadow-lg"
+                          >
+                            <ul className="text-secondary subtitle2 py-2">
+                              <li>
+                                <button
+                                  className="text-secondary px-4 py-2 text-left hover:bg-gray-100"
+                                  onClick={() => {
+                                    updateRole(
+                                      edge.node.id,
+                                      UserRole.CommitteeMember
+                                    );
+                                  }}
+                                >
+                                  Make as Comm Member
+                                </button>
+                              </li>
+                              {me?.role === UserRole.SystemAdministrator && (
+                                <li>
+                                  <button
+                                    className="text-secondary px-4 py-2 text-left hover:bg-gray-100"
+                                    onClick={() => {
+                                      updateRole(
+                                        edge.node.id,
+                                        UserRole.SystemAdministrator
+                                      );
+                                    }}
+                                  >
+                                    Make as System Admin
+                                  </button>
+                                </li>
+                              )}
+                              <li>
+                                <button
+                                  className="text-secondary px-4 py-2 text-left hover:bg-gray-100"
+                                  onClick={() => {
+                                    updateRole(edge.node.id, UserRole.User);
+                                  }}
+                                >
+                                  Make as Volunteer
+                                </button>
+                              </li>
+                              <li>
+                                <button className="px-4 py-2 text-left text-red-500 hover:bg-gray-100">
+                                  Delete User
+                                </button>
+                              </li>
+                            </ul>
+                          </div>
+                        )}
                       </td>
-                      {me?.role === UserRole.SystemAdministrator && (
-                        <>
-                          <td className="px-6 py-4">
-                            <button
-                              className="rounded-md bg-cyan-400 px-3 py-1 text-white hover:bg-cyan-500"
-                              onClick={() => {
-                                updateRole(edge.node.id, UserRole.User);
-                              }}
-                            >
-                              Make user
-                            </button>
-                            <button
-                              className="rounded-md bg-amber-400 px-3 py-1 text-white hover:bg-amber-500"
-                              onClick={() => {
-                                updateRole(
-                                  edge.node.id,
-                                  UserRole.CommitteeMember
-                                );
-                              }}
-                            >
-                              Make committee member
-                            </button>
-                            <button
-                              className="rounded-md bg-orange-400 px-3 py-1 text-white hover:bg-orange-500"
-                              onClick={() => {
-                                updateRole(
-                                  edge.node.id,
-                                  UserRole.SystemAdministrator
-                                );
-                              }}
-                            >
-                              Make system administrator
-                            </button>
-                          </td>
-                        </>
-                      )}
                     </tr>
                   ))}
                 </tbody>
