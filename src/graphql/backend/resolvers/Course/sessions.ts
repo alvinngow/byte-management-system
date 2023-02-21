271fac35f2309f519da9b514a45089a3d07f2396
Duncan Leo <hello@duncanleo.me>
2023-02-22 00:49:14 +0800

    feat(graphql): support sorting and filtering in `Course.sessions` [BIMST08-182] (#122)

--- src/graphql/backend/resolvers/Course/sessions.ts
+++ src/graphql/backend/resolvers/Course/sessions.ts
@@ -1,8 +1,16 @@
 import { findManyCursorConnection } from '@devoxa/prisma-relay-cursor-connection';
+import { DateTime } from 'luxon';
 
-import { CourseResolvers } from '../../../../../gen/graphql/resolvers';
+import {
+  CourseResolvers,
+  SessionDateFiltering,
+  SessionSortKey,
+} from '../../../../../gen/graphql/resolvers';
 import { prisma } from '../../../../db';
 import requireAuthenticated from '../util/requireAuthenticated';
+import { Prisma } from '.prisma/client';
+import SessionOrderByWithAggregationInput = Prisma.SessionOrderByWithAggregationInput;
+import SessionWhereInput = Prisma.SessionWhereInput;
 
 export const Course_sessionsResolver: CourseResolvers['sessions'] = async (
   root,
@@ -12,21 +20,61 @@ export const Course_sessionsResolver: CourseResolvers['sessions'] = async (
 ) => {
   await requireAuthenticated(context);
 
-  const { first, after } = args;
+  const { first, after, sortKey, filter, reverse } = args;
+
+  let orderBy: SessionOrderByWithAggregationInput | undefined = undefined;
+
+  switch (sortKey) {
+    case SessionSortKey.Start: {
+      orderBy = {
+        startDate: reverse ? 'desc' : 'asc',
+      };
+      break;
+    }
+    case SessionSortKey.End: {
+      orderBy = {
+        endDate: reverse ? 'desc' : 'asc',
+      };
+      break;
+    }
+  }
+
+  const where: SessionWhereInput = {
+    courseId: root.id,
+  };
+
+  if (filter != null) {
+    switch (filter.date) {
+      case SessionDateFiltering.Upcoming: {
+        where.OR = {
+          endDate: {
+            gte: DateTime.now().toJSDate(),
+          },
+        };
+        break;
+      }
+      case SessionDateFiltering.Past: {
+        where.OR = {
+          endDate: {
+            lt: DateTime.now().toJSDate(),
+          },
+        };
+        break;
+      }
+    }
+  }
 
   const result = await findManyCursorConnection(
     (args) =>
       prisma.session.findMany({
         ...args,
-        where: {
-          courseId: root.id,
-        },
+        where,
+        orderBy,
       }),
     () =>
       prisma.session.count({
-        where: {
-          courseId: root.id,
-        },
+        where,
+        orderBy,
       }),
     { first, after },
     { resolveInfo: info }
