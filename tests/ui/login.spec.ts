5508cc789bd4d46d30c96d4025679179e8547e60
Duncan Leo <hello@duncanleo.me>
2023-02-22 00:39:57 +0800

    build(ci): improve tests (#121)

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,12 +1,12 @@
 import { expect, Page, test } from '@playwright/test';
 
 async function login(page: Page, username: string, password: string) {
-  await page.goto('http://localhost:3000/login');
+  await page.goto('/login');
   await page.locator('input[name="email"]').fill(username);
   await page.locator('input[name="password"]').fill(password);
   await page.getByRole('button', { name: 'Log in' }).click();
 
-  await expect(page).toHaveURL('http://localhost:3000/home');
+  await expect(page).toHaveURL('/home');
   await expect(page.locator('h1')).toHaveText(
     'Discover Causes (Courses) That Matter To You'
   );
--- tests/ui/signup.spec.ts
+++ tests/ui/signup.spec.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 test('signup', async ({ page }) => {
   const username = `playwright-${uuidv4()}`;
 
-  await page.goto('http://localhost:3000/signup');
+  await page.goto('/signup');
   await page.locator('input[name="firstName"]').fill(username);
   await page.locator('input[name="lastName"]').fill('Smith');
   await page.locator('input[name="email"]').fill(`${username}@example.com`);
@@ -13,7 +13,7 @@ test('signup', async ({ page }) => {
   await page.locator('input[autocomplete="tel-local"]').fill('91234567');
   await page.getByRole('button', { name: 'Sign up' }).click();
 
-  await expect(page).toHaveURL('http://localhost:3000/home');
+  await expect(page).toHaveURL('/home');
   await expect(page.locator('h1')).toHaveText(
     'Discover Causes (Courses) That Matter To You'
   );
