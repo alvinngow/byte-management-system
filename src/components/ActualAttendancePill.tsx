3674d0c12962f048699494f4db63a7c09cfab981
kryeoh <89062427+kryeoh@users.noreply.github.com>
2023-03-11 13:40:35 +0800

    fix(ActualAttendancePill): Add warning to chip 'Absent' [BIMST08-242] (#176)

--- src/components/ActualAttendancePill.tsx
+++ src/components/ActualAttendancePill.tsx
@@ -1,16 +1,46 @@
+import 'react-tooltip/dist/react-tooltip.css';
+
+import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import React from 'react';
+import { Tooltip } from 'react-tooltip';
 
 import { Attendance } from '../../gen/graphql/resolvers';
+import IconButton from './IconButton';
 
 const classNameMap: Record<Attendance, string> = {
   [Attendance.Attend]: 'bg-blue-100 text-brand-main',
   [Attendance.Absent]: 'bg-amber-50 text-amber-500',
 };
 
-const textMap: Record<Attendance, string> = {
+const textMap: Record<Attendance, JSX.Element | string> = {
   [Attendance.Attend]: 'Attended',
-  [Attendance.Absent]: 'Absent',
+  [Attendance.Absent]: (
+    <>
+      <a id="warning-message" className="flex">
+        Absent
+        <IconButton
+          HeroIcon={() => (
+            <InformationCircleIcon className="ml-1 mb-1 bg-amber-50 text-amber-500" />
+          )}
+        />
+      </a>
+      <Tooltip
+        anchorSelect="#warning-message"
+        place="right"
+        style={{
+          width: '160px',
+          whiteSpace: 'normal',
+          padding: '15px 16px',
+          borderRadius: '10px',
+          background: 'rgb(75 85 99)',
+        }}
+      >
+        If you can&#39;t make it to the class, please kindly cancel your session
+        in advance.
+      </Tooltip>
+    </>
+  ),
 };
 
 interface Props {
