8a387342b0090888dd18fa77e8a8c7ea9afeeafa
Duncan Leo <hello@duncanleo.me>
2023-03-11 23:01:08 +0800

    fix(tests): Flaky unit tests (#181)

--- tests/unit/courseDelete.spec.ts
+++ tests/unit/courseDelete.spec.ts
@@ -1,10 +1,12 @@
 import { gql } from '@apollo/client';
+import { DateTime } from 'luxon';
 import { v4 as uuidv4 } from 'uuid';
 
 import {
   CourseDeleteInput,
   CourseDeletePayload,
 } from '../../gen/graphql/resolvers';
+import { prisma } from '../../src/db';
 import { buildMockContext } from '../util/buildMockContext';
 import setupApolloServer from '../util/setupApolloServer';
 
@@ -21,9 +23,37 @@ interface Variables {
 const USER_ID_COMMITTEE_MEMBER = '3a9a70e3-6116-4d33-b9ac-9da0de9c2778';
 const USER_ID_VOLUNTEER = 'f1444b1d-4360-42d4-bc1b-c23cd5051fa4';
 
-const COURSE_ID_SEEDED = '97fee544-baf2-4ffb-95c8-e3cf5e59b944';
-
 describe('courseDelete', () => {
+  let COURSE_ID_SEEDED = uuidv4();
+
+  /**
+   * Create a course to be deleted
+   */
+  beforeAll(async () => {
+    await prisma.course.create({
+      data: {
+        id: COURSE_ID_SEEDED,
+        name: 'Unit Test Course',
+        description: 'Course created by unit test',
+        descriptionPrivate: 'Hi there volunteer',
+        subtitle: 'Amazing unit test course',
+        defaultStartTime: DateTime.fromISO('19:00:00').toJSDate(),
+        defaultEndTime: DateTime.fromISO('21:30:00').toJSDate(),
+        defaultLocation: {
+          create: {
+            name: 'ABC Building',
+            description: '',
+            address: '123 ABC Street',
+            lat: 1,
+            lng: 103,
+            unit: '#01-01',
+            locationClusterId: 'd8bf4880-75d8-4443-869a-d9d96c3a5149',
+          },
+        },
+      },
+    });
+  });
+
   test('Volunteer cannot delete course', async () => {
     const clientMutationId = uuidv4();
 
