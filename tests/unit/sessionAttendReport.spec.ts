7ef01e6fc41aa2908fa8c3bee8f13158ff153c7f
Duncan Leo <hello@duncanleo.me>
2023-03-19 17:27:37 +0800

    fix(tests): flaky `sessionAttendReport` test (#213)

--- tests/unit/sessionAttendReport.spec.ts
+++ tests/unit/sessionAttendReport.spec.ts
@@ -1,4 +1,6 @@
 import { gql } from '@apollo/client';
+import bcrypt from 'bcrypt';
+import { DateTime } from 'luxon';
 import { v4 as uuidv4 } from 'uuid';
 
 import {
@@ -6,13 +8,11 @@ import {
   SessionAttendReportInput,
   SessionAttendReportPayload,
 } from '../../gen/graphql/resolvers';
+import { prisma } from '../../src/db';
 import { buildMockContext } from '../util/buildMockContext';
 import setupApolloServer from '../util/setupApolloServer';
 
 const USER_ID_COMMITTEE_MEMBER = '3a9a70e3-6116-4d33-b9ac-9da0de9c2778';
-const USER_ID_VOLUNTEER = '28edcad7-da77-456c-b2a1-9e20470e058a';
-const SESSION_ID_SEEDED = '5141c5c9-ad0a-451a-bbba-409dc0724cf0';
-
 const testServer = setupApolloServer();
 
 interface Data {
@@ -36,6 +36,68 @@ const Mutation = gql`
 `;
 
 describe('sessionAttendReport', () => {
+  const USER_ID_VOLUNTEER_GENERATED = uuidv4();
+  const SESSION_ID_GENERATED = uuidv4();
+
+  beforeAll(async () => {
+    // Create a user
+    const hashedPassword = await bcrypt.hash('password', 10);
+
+    await prisma.user.create({
+      data: {
+        id: USER_ID_VOLUNTEER_GENERATED,
+        email: `user-${uuidv4()}-unit-test@example.com`,
+        pwHash: hashedPassword,
+        firstName: 'John',
+        lastName: 'Smith',
+        mobileNo: '+65 91234567',
+      },
+    });
+
+    // Create a course with session
+    await prisma.course.create({
+      data: {
+        name: 'Unit Test Course',
+        description: 'Course created by unit test',
+        descriptionPrivate: 'Hi there volunteer',
+        subtitle: 'Amazing unit test course',
+        defaultStartTime: DateTime.fromISO('19:00:00').toJSDate(),
+        defaultEndTime: DateTime.fromISO('21:30:00').toJSDate(),
+        defaultLocation: {
+          connectOrCreate: {
+            where: {
+              name: 'ABC Building',
+            },
+            create: {
+              name: 'ABC Building',
+              description: '',
+              address: '123 ABC Street',
+              lat: 1,
+              lng: 103,
+              unit: '#01-01',
+              locationClusterId: 'd8bf4880-75d8-4443-869a-d9d96c3a5149',
+            },
+          },
+        },
+        sessions: {
+          create: {
+            id: SESSION_ID_GENERATED,
+            name: '',
+            description: '',
+            startDate: DateTime.now().toJSDate(),
+            endDate: DateTime.now().toJSDate(),
+            sessionAttendees: {
+              create: {
+                userId: USER_ID_VOLUNTEER_GENERATED,
+                indicatedAttendance: Attendance.Attend,
+              },
+            },
+          },
+        },
+      },
+    });
+  });
+
   test('committee member can report on attendance', async () => {
     const clientMutationId = uuidv4();
     const actualAttendance = Attendance.Absent;
@@ -46,8 +108,8 @@ describe('sessionAttendReport', () => {
         variables: {
           input: {
             clientMutationId,
-            sessionId: SESSION_ID_SEEDED,
-            userId: USER_ID_VOLUNTEER,
+            sessionId: SESSION_ID_GENERATED,
+            userId: USER_ID_VOLUNTEER_GENERATED,
             actualAttendance,
           },
         },
@@ -89,14 +151,14 @@ describe('sessionAttendReport', () => {
         variables: {
           input: {
             clientMutationId,
-            sessionId: SESSION_ID_SEEDED,
-            userId: USER_ID_VOLUNTEER,
+            sessionId: SESSION_ID_GENERATED,
+            userId: USER_ID_VOLUNTEER_GENERATED,
             actualAttendance,
           },
         },
       },
       {
-        contextValue: buildMockContext(USER_ID_VOLUNTEER),
+        contextValue: buildMockContext(USER_ID_VOLUNTEER_GENERATED),
       }
     );
 
@@ -118,8 +180,8 @@ describe('sessionAttendReport', () => {
         variables: {
           input: {
             clientMutationId,
-            sessionId: SESSION_ID_SEEDED,
-            userId: USER_ID_VOLUNTEER,
+            sessionId: SESSION_ID_GENERATED,
+            userId: USER_ID_VOLUNTEER_GENERATED,
             actualAttendance,
           },
         },
