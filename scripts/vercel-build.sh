230804adf984d3e6ceabec42502544c16fd2d5c9
Duncan Leo <hello@duncanleo.me>
2023-04-15 16:11:32 +0800

    fix(vercel): run different commands depending on env (#290)

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
 yarn build
-DATABASE_URL=$DATABASE_NONPOOL_URL yarn prod:db:migrate
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "production" ]] ; then
+  echo "[VERCEL BUILD] Performing production database migration"
+  DATABASE_URL=$DATABASE_NONPOOL_URL yarn prod:db:migrate
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  echo "[VERCEL BUILD] Performing staging database migration & seed"
+  DATABASE_URL=$DATABASE_NONPOOL_URL yarn prod:db:migrate
+  DATABASE_URL=$DATABASE_NONPOOL_URL yarn db:seed
+else
+  echo "[VERCEL BUILD] Performing preview database reset (incl. migration and seed)"
+  DATABASE_URL=$DATABASE_NONPOOL_URL yarn db:reset --force
+  DATABASE_URL=$DATABASE_NONPOOL_URL yarn db:seed
+fi
